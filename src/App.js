eb59b673487653c5fa95b97c4563c1c709bd650b
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-12-03 22:48:48 -0500

    Creating 'TextParagraph' component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Title from './Components/Title/Title';
 import Subtitle from './Components/Subtitle/Subtitle';
+import TextParagraph from './Components/TextParagraph/TextParagraph';
 
 function App() {
   return (
     <div className="App">
       <Title title="The creative crew"/>
       <Subtitle subtitle="Who we are" />
+      <TextParagraph />
     </div>
   );
 }
---
+++ src/Components/TextParagraph/TextParagraph.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './TextParagraph.css'
+
+class TextParagraph extends React.Component {
+  render() {
+    return (
+      <div className="textParagraph">
+        We are team of creatively diverse. driven.
+        innovative individuals working in various
+        locations from the world.
+      </div>
+    )
+  }
+}
+
+export default TextParagraph;
