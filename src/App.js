8da037be8ba0c0e3d90d2d350dd73bbc38e7eb76
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-12-04 21:56:18 -0500

    Removing some component from this view

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
-import Title from './Components/Title/Title';
-import Subtitle from './Components/Subtitle/Subtitle';
-import TextParagraph from './Components/TextParagraph/TextParagraph';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <Title title="The creative crew"/>
-      <Subtitle subtitle="Who we are" />
-      <TextParagraph />
+      <Header />
     </div>
   );
 }
