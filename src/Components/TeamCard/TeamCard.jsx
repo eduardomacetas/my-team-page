0c4a2e594db985af016e3832302aed43e7d60840
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-12-09 08:00:45 -0500

    Adding css to my 'TeaamHeader' component

--- src/Components/TeamCard/TeamCard.jsx
+++ src/Components/TeamCard/TeamCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import './TeamCard.css';
 
 class TeamCard extends React.Component {
   render() {
     return (
-      <div>
-        TeamCard
+      <div class="teamCard">
+        <div class="upper-container">
+          <img src="" alt="" height="350px" width="250px"/>
+          <div className="use-role">
+            Product Owner
+          </div>
+        </div>
+        <div class="lower-container">
+          Eduardo Macetas
+        </div>
       </div>
     )
   }
