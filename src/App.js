25e143da6c35eb184df4dd91582f4c1f0af38033
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-12-10 22:45:40 -0500

    Removing 'TeamCard' component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import SectionTeam from './Components/SectionTeam/SectionTeam';
-import TeamCard from './Components/TeamCard/TeamCard';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <SectionTeam />
-      <TeamCard />
     </div>
   );
 }
