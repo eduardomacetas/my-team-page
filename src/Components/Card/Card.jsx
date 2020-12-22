ee9ac25ef8b5ea4dcc78ed2b94e677024d67389d
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-12-22 08:10:25 -0500

    Changing "card" by "teamCard" and code formatting

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 class Card extends React.Component {
   render() {
     return (
-      <div class="teamCard">
+      <div class="card">
         <div class="upper-container">
-          <img src="" alt="" height="350px" width="250px"/>
-          <div className="use-role">
-            Product Owner
-          </div>
+          <img src="" alt="" height="350px" width="250px" />
+          <div className="user-role">Product Owner</div>
         </div>
-        <div class="lower-container">
-          Eduardo Macetas
-        </div>
+        <div class="lower-container">Eduardo Macetas</div>
       </div>
-    )
+    );
   }
 }
 
