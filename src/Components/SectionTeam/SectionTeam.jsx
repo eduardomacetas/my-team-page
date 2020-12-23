7695a2372917aa6bc9b57c3846a6679099520838
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-12-22 23:23:18 -0500

    Adding props

--- src/Components/SectionTeam/SectionTeam.jsx
+++ src/Components/SectionTeam/SectionTeam.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
-import Card from '../Card/Card';
+import React from "react";
+import Card from "../Card/Card";
+import "./SectionTeam.css";
 
 class SectionTeam extends React.Component {
   render() {
     return (
-      <div>
-        Team!
-        <Card />
+      <div class="team-section">
+        <div class="row">
+          <div class="column">
+            <Card />
+          </div>
+          <div class="column">
+            <Card />
+          </div>
+          <div class="column">
+            <Card />
+          </div>
+        </div>
+        <div class="row">
+          <div class="column">
+            <Card />
+          </div>
+          <div class="column">
+            <Card />
+          </div>
+          <div class="column">
+            <Card />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
