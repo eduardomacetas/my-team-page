7115acbc8b91f63aae6485bdc8209b3f71663127
Eduardo Macetas <eduardo.macetas@gmail.com>
2020-12-26 22:44:20 -0500

    Code formatting

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import './Header.css';
-import Title from '../Title/Title';
-import Subtitle from '../Subtitle/Subtitle';
-import TextParagraph from '../TextParagraph/TextParagraph';
+import React from "react";
+import "./Header.css";
+import Title from "../Title/Title";
+import Subtitle from "../Subtitle/Subtitle";
+import TextParagraph from "../TextParagraph/TextParagraph";
 
 class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <div className="row">
-          <Title title="The creative crew"/>
+        <div className="column">
+          <Title title="The creative crew" />
         </div>
-        <div className="row">
-          <Subtitle subtitle="Who we are"/>
+        <div className="column">
+          <Subtitle subtitle="Who we are" />
           <TextParagraph />
         </div>
       </div>
-    )
+    );
   }
 }
 
